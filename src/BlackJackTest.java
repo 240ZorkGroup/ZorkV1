eb7abdd25fadfbab420bc54cd7d944cd7870b6e3
Billy Andrews <wandrews@umw.edu>
2016-11-09 17:45:52 -0500

    Added TICTACTOE CAPABILITIES!!!2

--- src/BlackJackTest.java
+++ src/BlackJackTest.java
@@ -1,7 +1,14 @@
 /**
- * Created by willi on 11/9/2016.
+ * TIC TAC TOE! This is a tester class to just play the Tic Tac Toe GUI game.
+ * @author Billy Andrews
+ * @version 20161109
  */
 public class BlackJackTest {
+
+    /**
+     * Main Tester class
+     * @param args
+     */
 
     public static void main(String[] args){
         System.out.println("This is going to test the blackjack capabilities");
