506a26554320e3975abc3485d81ca91207657ada
Billy Andrews <wandrews@umw.edu>
2016-11-09 16:06:14 -0500

    Added DropCommand's javadoc.

--- src/DropCommand.java
+++ src/DropCommand.java
@@ -1,13 +1,26 @@
 //package zeitz_borkv3;
 
+/**
+ * DropCommand drops an item from the player's inventory
+ * @author Billy Andrews, Kelly Morgan, Maryfay Jackson
+ * @version 20161109
+ */
 class DropCommand extends Command {
 
     private String itemName;
 
+    /**
+     * Constructor for DropCommand
+     * @param itemName Item that you want to be dropped.
+     */
     DropCommand(String itemName) {
         this.itemName = itemName;
     }
 
+    /**
+     * Execute method runs the command.
+     * @return Visual confirmation that the item has been dropped from the player's inventory.
+     */
     public String execute() {
         if (itemName == null || itemName.trim().length() == 0) {
             return "Drop what?\n";
