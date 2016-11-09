915d75831a350490ec9119e3f3a580d5c70b757c
Billy Andrews <wandrews@umw.edu>
2016-11-09 15:12:27 -0500

    Added VerboseCommand's javadoc.

--- src/VerboseCommand.java
+++ src/VerboseCommand.java
@@ -1,16 +1,21 @@
 /**
- *
+ * VerboseCommand displays the room description if toggled on, otherwise it only displays the first time.
+ * @author Billy Andrews, Kelly Morgan, Maryfay Jackson
+ * @version 20161109
  */
     class VerboseCommand extends Command {
 
         private boolean verboseToggle = false;
 
+    /**
+     * Constructor for VerboseCommand.
+     */
         VerboseCommand() {
         }
 
         /**
-         *
-         * @return
+         * Execute method runs the command.
+         * @return Visual confirmation that the verbose mode has been toggled on/off.
          */
         public String execute() {
 
