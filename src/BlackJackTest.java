22f7c3b93cc006495590383df0db2584ec35e020
Billy Andrews <wandrews@umw.edu>
2016-11-09 17:42:01 -0500

    Added TICTACTOE CAPABILITIES!!!

---
+++ src/BlackJackTest.java
@@ -0,0 +1,11 @@
+/**
+ * Created by willi on 11/9/2016.
+ */
+public class BlackJackTest {
+
+    public static void main(String[] args){
+        System.out.println("This is going to test the blackjack capabilities");
+        NPC grue = new NPC("Grue");
+        grue.converse();
+    }
+}
--- src/NPC.java
+++ src/NPC.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * NPC - Non-Player Characters
  * This class provides an NPC that the player can interact with.
@@ -6,22 +8,45 @@
  */
 public class NPC {
 
-    private String name;
+    Scanner in = new Scanner(System.in);
+
+    private String monsterName;
+    private String playerName;
     /**
      * Constructor class for NPCs will create a named NPC class.
      * @param n The name of the NPC. Choose whatever you want.
      */
     public NPC(String n){
-        this.name = n;
+        this.monsterName = n;
     }
 
     /**
-     * Converse method allows the player to hold a conversation with the NPC.
+     * Converse method allows the player to play a game (within a game) with the NPC.
      */
     //TODO write method.
     public void converse(){
 
-        System.out.println("Hey! This is where the NPC Speaks!");
+        System.out.println("As you were examining the room, an ugly beast appears. \n" +
+                "Frightened, you reach for anything to use as a weapon. \n\n" +
+                "'Don't be alarmed!!' it shouted. 'I only want to play a game with you. \n" +
+                "It gets lonely in this dungeon of a basement... My name is " + monsterName + ".'\n\n" +
+                "The beast seems harmless.\n");
+
+        System.out.println("'So what do you say? A quick game of Tic-Tac-Toe?'");
+
+        if (in.nextLine().equalsIgnoreCase("yes") || in.nextLine().equalsIgnoreCase("y")){
+            System.out.println(monsterName + ": 'Great! I don't think I caught your name: '");
+            this.playerName = in.nextLine();
+            System.out.println(monsterName + ": Well nice to meet you, " + playerName + ". ");
+
+            TTT tictactoe = new TTT();
+            tictactoe.setVisible(true);
+
+        } else {
+            System.out.println("'I don't have time for that,' you reply.");
+            System.out.println("'Maybe next time.'");
+            System.out.println();
+        }
     }
 
 
